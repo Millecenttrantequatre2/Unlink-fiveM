bab2ec4f52bee2b21312815465f40100bc70e0f6
1134 <168791407+Millecenttrantequatre2@users.noreply.github.com>
2024-05-05 02:16:11 +0200

    setup.py

---
+++ CLEANER fiveM/setup.py
@@ -0,0 +1,12 @@
+import os 
+import sys
+import platform
+
+print("Installations des modules pythons requis:")
+if sys.platform.startswith("win"):
+    "WINDOWS"
+    os.system("pip install --upgrade pip install pip install colorama")
+
+if sys.platform.startswith("linux"):
+    "LINUX"
+    os.system("pip install --upgrade pip install pip install colorama")
